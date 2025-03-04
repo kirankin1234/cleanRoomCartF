3081e857ef8159f3a264f793e8c068bf61187dec
Kiran Gaikwad <kiran899964@gmail.com>
2025-03-04 10:59:14 +0530

    base url

--- src/pages/admin/auth/AdminSignup.jsx
+++ src/pages/admin/auth/AdminSignup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Input, Button, Card, message } from 'antd';
 import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
-import BaseURL from '../../../API/BaseURL';
+import {BASE_URL} from '../../../API/BaseURL';
 
 const AdminSignup = () => {
   const navigate = useNavigate();
